7a74419396b8ff7ca2c845364497fcc5429abd07
Mitali Salvi <mits.salvi93@gmail.com>
2018-12-01 21:21:08 -0500

    Added methods for crossover and mutate in GeneticAlgorithm.java

--- FootballFixtures/src/main/java/fixtures/Constants.java
+++ FootballFixtures/src/main/java/fixtures/Constants.java
@@ -9,12 +9,14 @@ package main.java.fixtures;
  */
 public class Constants 
 {
-	public static final int POPULATION_SIZE = 50;
+	public static final int POPULATION_SIZE = 100;
 	
 	public static final int NUMBER_OF_ROUNDS = 2; 
 	
 	public static final int ELITE_FACTOR = 5;
 	
 	public static final int K_FACTOR = 3;  //keep 5 for population 100
+	
+	public static final double MUTATION_FACTOR = 0.05;
 
 }
--- FootballFixtures/src/main/java/fixtures/FixturesMain.java
+++ FootballFixtures/src/main/java/fixtures/FixturesMain.java
@@ -1,5 +1,34 @@
 package main.java.fixtures;
 
-public class FixturesMain {
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FixturesMain 
+{
+	public static void main(String[] args) 
+	{
+		//Give input to the Fixture Generator
+		Configuration.initializeData();
+		
+		//Initialize initial population and calculate and set fitness of each chromosome
+		Population initialPopulation = new Population(3);	
+		
+		//Sort the initial population on the basis of fitness factor
+		Arrays.sort(initialPopulation.getChromosomes(), new Comparator<Chromosome>() {
+	        public int compare(Chromosome o1, Chromosome o2) {
+	            return o1.compareTo(o2);
+	        }
+	    });
+		
+//		Chromosome temp[] = initialPopulation.getChromosomes();
+//		for (int i=0;i<temp.length;i++)
+//		{
+//			System.out.println(temp[i] +"fitness::::"+temp[i].getFitness());
+//		}
+		
+		//call k 
+
+	}
+
 
 }
