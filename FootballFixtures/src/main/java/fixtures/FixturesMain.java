69d2eac93d742132afb0a754bffbe893ca419c1d
Aditi <jalkote.a@husky.neu.edu>
2018-12-02 15:52:23 -0500

    code to bridge gap between parentSelection, crossor, mutation

--- FootballFixtures/src/main/java/fixtures/FixturesMain.java
+++ FootballFixtures/src/main/java/fixtures/FixturesMain.java
@@ -3,8 +3,10 @@ package main.java.fixtures;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class FixturesMain 
-{
+public class FixturesMain {
+	
+	private static GeneticAlgorithm geneticAlgo;
+	
 	public static void main(String[] args) 
 	{
 		//Give input to the Fixture Generator
@@ -12,13 +14,8 @@ public class FixturesMain
 		
 		//Initialize initial population and calculate and set fitness of each chromosome
 		Population initialPopulation = new Population(3);	
+		geneticAlgo.runGeneticAlgorithm(initialPopulation);
 		
-		//Sort the initial population on the basis of fitness factor
-		Arrays.sort(initialPopulation.getChromosomes(), new Comparator<Chromosome>() {
-	        public int compare(Chromosome o1, Chromosome o2) {
-	            return o1.compareTo(o2);
-	        }
-	    });
 		
 //		Chromosome temp[] = initialPopulation.getChromosomes();
 //		for (int i=0;i<temp.length;i++)
